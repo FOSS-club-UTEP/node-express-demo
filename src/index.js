e72415c7ee7f9d0e08e7fc7c24b4f0e6f4fefa9e
AshkanArabim <ashkan.arabim@gmail.com>
2024-10-29 11:54:28 -0600

    add about page

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../static/index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../static/about.html'))
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
